eaa022f9be452937ce5d59ddf296069d838cc4d2
Rachelle Lee <rachedeveloper@gmail.com>
2021-08-02 18:45:28 -0700

    Initial Commit

---
+++ SnakeLikeEffect/SnakeLikeEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeLikeEffectApp.swift
+//  SnakeLikeEffect
+//
+//  Created by RachelleLee on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SnakeLikeEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
